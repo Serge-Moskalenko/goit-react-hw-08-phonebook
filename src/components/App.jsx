285637c695dc900e16dc692f4ca69aa17e13c5cc
Serge-Moskalenko <skmos2019@outlook.com>
2022-12-07 15:14:05 +0000

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,31 @@ import { Contacts } from "./Contacts";
 export class App extends Component {
   state = {
     contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    filter:'',
+    filter: '',
+  }
+
+  componentDidMount() {
+
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contacts) {
+       this.setState({contacts})
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+
+    const { contacts } = this.state;
+
+    if (prevState.contacts !== contacts) {
+
+      localStorage.setItem('contacts', JSON.stringify(contacts))
+    } 
   }
 
  formDataHendler = data => {
