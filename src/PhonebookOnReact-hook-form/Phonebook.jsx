0ba14270c73826398ea4e45b6845ee8c1a4b5592
Serge-Moskalenko <skmos2019@outlook.com>
2023-01-20 10:51:01 +0000

    hook-form

--- src/PhonebookOnReact-hook-form/Phonebook.jsx
+++ src/PhonebookOnReact-hook-form/Phonebook.jsx
@@ -1,43 +1,23 @@
-import { useState } from 'react';
+import {useForm} from 'react-hook-form'
 import { useAddContactMutation } from 'redux/contactSlice';
 
 export const Phonebook = () => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+
+    const { register, handleSubmit,reset } = useForm();
 
     const [addContact] = useAddContactMutation();
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-
-    switch (name) {
-      case 'name':
-        setName(value);
-        break;
-
-      case 'number':
-        setNumber(value);
-        break;
-      
-      default: console.log(name)
-    }
-  };
-
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    
-      addContact({ name, number })
-    setName('');
-    setNumber('');
-    
+    const handleClickSubmit = ({name,number}) =>{
+      addContact({ name, number });
+      reset();
+        
   };
 
   return (
     <>
       <h1>Phonebook</h1>
         
-      <form onSubmit={handleSubmit} style={{
+      <form onSubmit={handleSubmit(handleClickSubmit)} style={{
         border: '2px solid black',
         padding: '20px',
         width: "400px"
@@ -48,20 +28,20 @@ export const Phonebook = () => {
           fontSize: "16px",
         }}>
           Name
-          <input style={{
-            width: "200px",
-            height: "20px",
-            marginTop: '10px'
-          }}
-            type="text"
-            name="name"
-            value={name}
+                  <input style={{
+                      width: "200px",
+                      height: "20px",
+                      marginTop: '10px'
+                  }}
+                      type="text"
+                      {...register("name")}
+         
             placeholder="Only text"
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
             maxLength={20}
-            onChange={handleChange}
+      
           />
         </label>
 
@@ -77,14 +57,14 @@ export const Phonebook = () => {
             marginTop: '10px'
           }}
             type="tel"
-            name="number"
-            value={number}
+            {...register("number")}
+           
             placeholder="Only number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
             maxLength={20}
-            onChange={handleChange}
+        
           />
         </label>
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-import { Phonebook } from "./Phonebook";
-import { Contacts } from "./Contacts/Contacts";
+import { Routes, Route } from "react-router-dom";
+import { SharedLayout } from "pages/SharedLayout/SharedLayout";
+import { lazy } from "react";
 
-export function App() {
+const Home = lazy(() => import('../pages/Home/Home'));
+// const Movies = lazy(() => import('../pages/Movies/Movies'));
+// const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
+// const Cast = lazy(() => import('./Cast'));
+// const Reviews = lazy(() => import('./Reviews'));
 
+export const App = () => {
   return (
-    <div
-        style={{
-        marginLeft:'20px'
-        }}>
-        
-      <Phonebook  />
-      <Contacts />
-      
-      </div>)
+    <Routes>
+      <Route path='/' element={<SharedLayout />} >
+        <Route index element={<Home />} />
+        {/* <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/contacts' element={<Contacts />} /> */}
+      </Route>
+    </Routes>
+  );
 };
+
+
+// import { Phonebook } from "PhonebookOnReact-hook-form/Phonebook";
+// import { Contacts } from "./Contacts/Contacts";
+
+// export function App() {
+
+//   return (
+//     <div
+//         style={{
+//         marginLeft:'20px'
+//         }}>
+        
+//       <Phonebook  />
+//       <Contacts />
+      
+//       </div>)
+// };
